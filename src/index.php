3d8b0a5900d62577351f364fee52ceb54f10a476
Damon Lynn <damon.lynn@fahlgren.com>
2018-07-27 13:30:11 -0400

    initial cleanup stuff

--- src/index.php
+++ src/index.php
@@ -10,14 +10,15 @@
   <body>
     <section class="main-container">
         <section class="container">
-            <div class="tout">
+            <div class="tout" style="background-image:url(/images/tout-bg.jpg);">
                 <h1>h1</h1>
                 <i class="far fa-arrow-alt-circle-down"></i>
-                <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me? " he thought.</p>
+                <p>One morning, when <a href="#">Gregor Samsa</a> woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me? " he thought.</p>
             </div>
 
             <div class="container__flex">
                 <div class="tout tout__flex">
+                    <input type="text" placeholder="placeholder" />
                     <button class="btn">Button</button>
                     <a href="#">Link</a>
                 </div>
